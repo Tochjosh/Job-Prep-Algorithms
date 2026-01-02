a146b2fe0bf4faf1eae5c6a71457b8c49183f816
tochjosh <tochjosh@gmail.com>
2026-01-01 22:54:20 -0600

    two_sum

---
+++ array/two_sum.py
@@ -0,0 +1,51 @@
+
+
+def two_sum(nums: list[int], target: int) -> list[int]:
+    """
+    Problem:
+    Given an array of integers `nums` and an integer `target`, return the
+    indices of the two numbers such that they add up to `target`.
+
+    You may assume that each input has exactly one solution, and you may not
+    use the same element twice.
+
+    You can return the answer in any order.
+
+    Examples:
+    Example 1:
+        Input: nums = [2, 7, 11, 15], target = 9
+        Output: [0, 1]
+        Explanation:
+            nums[0] + nums[1] == 9, so we return [0, 1].
+
+    Example 2:
+        Input: nums = [3, 2, 4], target = 6
+        Output: [1, 2]
+
+    Example 3:
+        Input: nums = [3, 3], target = 6
+        Output: [0, 1]
+
+    Constraints:
+        2 <= nums.length <= 10^4
+        -10^9 <= nums[i] <= 10^9
+        -10^9 <= target <= 10^9
+        Exactly one valid answer exists.
+
+    Follow-up:
+        Can you come up with an algorithm that runs in less than O(n^2) time
+        complexity?
+    """
+
+    seen = {}
+
+    for index, num in enumerate(nums):
+        diff = target - num
+        if diff in seen:
+            return [seen[diff], index]
+        seen[num] = index
+    return []
+
+
+print(two_sum([2, 7, 11, 15], 9))
+
