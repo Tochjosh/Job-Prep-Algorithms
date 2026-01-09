a9ba32e7a4ac8347d4b787dee7dc5aa189e1b0d9
tochjosh <tochjosh@gmail.com>
2026-01-08 22:31:33 -0600

    search revised

--- array/search.py
+++ array/search.py
@@ -1,7 +1,7 @@
 
 
 
-def search(nums: list[int], target: int) -> int:
+def search(nums: list[int], target: int):
     """
     Problem:
     There is an integer array `nums` sorted in ascending order with distinct
@@ -43,17 +43,29 @@ def search(nums: list[int], target: int) -> int:
 
     left, right = 0, len(nums) - 1
 
-    while left < right:
+    while left <= right:
         mid = (left + right) // 2
 
         if nums[mid] == target:
             return mid
-        if target > nums[right]:
-            left = mid + 1
-        else:
-            right = mid
 
-    return left if nums[left] == target else -1
+        # the left half is sorted
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        # the right half is sorted
+        else:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+    return -1
+
 
 
 print(search([4, 5, 6, 7, 0, 1, 2], 0))
+print(search([1], 0))
+print(search([4, 5, 6, 7, 0, 1, 2], 3))
